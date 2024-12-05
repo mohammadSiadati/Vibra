1ba2fb73700a8137cd0cf7c83fd46e7e21538a71
Mohammad siadati <msiadati77@gmail.com>
2024-12-05 21:30:01 +0330

    fixed build error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,9 @@
-import type { Metadata } from 'next';
-import localFont from 'next/font/local';
-import './globals.css';
+import type { Metadata } from "next";
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: 'Vibra',
-  description: 'enjoy',
+  title: "Vibra",
+  description: "enjoy",
 };
 
 export default function RootLayout({
